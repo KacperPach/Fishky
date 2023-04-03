e095c582701d396808031909a48094416d40eb2d
s27112-pj <115423896+Kacper-Pach@users.noreply.github.com>
2023-04-03 20:59:04 +0200

    initial angular setup

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'fishky-quiz',
+    appId: '1:601441781308:web:4841f015313dfcc10ee539',
+    storageBucket: 'fishky-quiz.appspot.com',
+    apiKey: 'AIzaSyDCeMa6NFtAX-7vhfz8ug2f-sT3qSbqcug',
+    authDomain: 'fishky-quiz.firebaseapp.com',
+    messagingSenderId: '601441781308',
+    measurementId: 'G-LRYD056VRX',
+  }
+}
